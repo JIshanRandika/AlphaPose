50fc923d1c0080d499d2dd1656e9651f9d8957fd
JIshanRandika <jishanrandika@gmail.com>
2024-07-28 15:51:45 +0530

    update

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -5,7 +5,7 @@ CKPT=$2
 VIDEO=$3
 OUTDIR=${4:-"./examples/res"}
 
-python scripts/demo_inference.py \
+python demo_inference.py \
     --cfg ${CONFIG} \
     --checkpoint ${CKPT} \
     --video ${VIDEO} \
